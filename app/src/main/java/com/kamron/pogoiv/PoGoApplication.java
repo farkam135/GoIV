f0732f14f736664cf4caf7b9068300c8c2c8a839
Henry Larkin <hfglarkin@gmail.com>
2016-08-20 19:06:34 +0100

    added offline flavour that doesn't use the internet permission

---
+++ app/src/main/java/com/kamron/pogoiv/PoGoApplication.java
@@ -0,0 +1,20 @@
+package com.kamron.pogoiv;
+
+import android.app.Application;
+
+import timber.log.Timber;
+
+public class PoGoApplication extends Application {
+
+    @Override public void onCreate() {
+        super.onCreate();
+
+        CrashlyticsWrapper.init(getApplicationContext());
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        } else {
+            Timber.plant(new CrashlyticsWrapper.CrashReportingTree());
+        }
+    }
+}

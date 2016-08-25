c59528a97b80b266780922a7bf243c253f690a00
Hari <rhari991@gmail.com>
2016-08-25 18:01:51 +0530

    Changed how the updater works

--- app/src/main/java/com/kamron/pogoiv/updater/AppUpdateLoader.java
+++ app/src/main/java/com/kamron/pogoiv/updater/AppUpdateLoader.java
@@ -3,7 +3,6 @@ package com.kamron.pogoiv.updater;
 import com.kamron.pogoiv.BuildConfig;
 
 import org.greenrobot.eventbus.EventBus;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -18,27 +17,19 @@ public class AppUpdateLoader extends Thread {
     @Override
     public void run() {
         OkHttpClient httpClient = new OkHttpClient();
-        String apiEndpoint = "https://api.github.com/repos/farkam135/GoIV/releases";
+        String apiEndpoint = "https://api.github.com/repos/farkam135/GoIV/releases/latest";
         Request request = new Request.Builder()
                 .url(apiEndpoint)
                 .build();
 
         try {
             Response response = httpClient.newCall(request).execute();
-            JSONArray releaseInfo = new JSONArray(response.body().string());
+            JSONObject releaseInfo = new JSONObject(response.body().string());
+            JSONObject releaseAssets = releaseInfo.getJSONArray("assets").getJSONObject(0);
+            if(releaseAssets.getString("name").contains("Offline"))
+                releaseAssets = releaseInfo.getJSONArray("assets").getJSONObject(1);
 
-            //Loop through the releases till we get to the first online release
-            JSONObject latestOnlineRelease = releaseInfo.getJSONObject(0);
-            for(int i = 0;i < releaseInfo.length();i++){
-                JSONObject latestRelease = releaseInfo.getJSONObject(i);
-                if(!latestRelease.getString("tag_name").contains("(Offline)")) {
-                    latestOnlineRelease = latestRelease;
-                    break;
-                }
-            }
-            JSONObject releaseAssets = latestOnlineRelease.getJSONArray("assets").getJSONObject(0);
-
-            AppUpdate update = new AppUpdate(releaseAssets.getString("browser_download_url"), latestOnlineRelease.getString("tag_name"), latestOnlineRelease.getString("body"));
+            AppUpdate update = new AppUpdate(releaseAssets.getString("browser_download_url"), releaseInfo.getString("tag_name"), releaseInfo.getString("body"));
 
             SemVer currentVersion = SemVer.parse(BuildConfig.VERSION_NAME);
             SemVer remoteVersion = SemVer.parse(update.getVersion());

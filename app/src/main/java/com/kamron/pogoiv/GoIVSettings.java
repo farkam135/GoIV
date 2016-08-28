0c600f05a604a8c469326ca3578b8da2b9716893
Henry Larkin <hfglarkin@gmail.com>
2016-08-28 03:03:06 +0100

    remove unnecessary sharedprefs writes from settings

--- app/src/main/java/com/kamron/pogoiv/GoIVSettings.java
+++ app/src/main/java/com/kamron/pogoiv/GoIVSettings.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
@@ -19,6 +18,8 @@ public class GoIVSettings {
     private boolean sendCrashReports;
     private boolean autoUpdateEnabled;
 
+    public static final String PREFS_GO_IV_SETTINGS = "GoIV_settings";
+
     public static final String LAUNCH_POKEMON_GO = "launchPokemonGo";
     public static final String SHOW_CONFIRMATION_DIALOG = "showConfirmationDialog";
     public static final String MANUAL_SCREENSHOT_MODE = "manualScreenshotMode";
@@ -27,23 +28,8 @@ public class GoIVSettings {
     public static final String SEND_CRASH_REPORTS = "sendCrashReports";
     public static final String AUTO_UPDATE_ENABLED = "autoUpdateEnabled";
 
-    public static void saveSettings(Context context, GoIVSettings newSettings) {
-        SharedPreferences settingsPreferences = context.getSharedPreferences("GoIV_settings", Context.MODE_PRIVATE);
-        SharedPreferences.Editor settingsEditor = settingsPreferences.edit();
-
-        settingsEditor.putBoolean(LAUNCH_POKEMON_GO, newSettings.getLaunchPokemonGo());
-        settingsEditor.putBoolean(SHOW_CONFIRMATION_DIALOG, newSettings.getShowConfirmationDialog());
-        settingsEditor.putBoolean(MANUAL_SCREENSHOT_MODE, newSettings.getManualScreenshotMode());
-        settingsEditor.putBoolean(DELETE_SCREENSHOTS, newSettings.getDeleteScreenshots());
-        settingsEditor.putBoolean(COPY_TO_CLIPBOARD, newSettings.getCopyToClipboard());
-        settingsEditor.putBoolean(SEND_CRASH_REPORTS, newSettings.getSendCrashReports());
-        settingsEditor.putBoolean(AUTO_UPDATE_ENABLED, newSettings.getAutoUpdateEnabled());
-
-        settingsEditor.apply();
-    }
-
     public static GoIVSettings getSettings(Context context) {
-        SharedPreferences settingsPreferences = context.getSharedPreferences("GoIV_settings", Context.MODE_PRIVATE);
+        SharedPreferences settingsPreferences = context.getSharedPreferences(PREFS_GO_IV_SETTINGS, Context.MODE_PRIVATE);
 
         return new GoIVSettings(settingsPreferences.getBoolean(LAUNCH_POKEMON_GO, true),
                 settingsPreferences.getBoolean(SHOW_CONFIRMATION_DIALOG, true),

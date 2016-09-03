01f6668510203ced648b527adbcd37320d075e06
DJCrapsody <deejaycrapsody@gmail.com>
2016-09-04 01:54:42 +0800

    Add marker for max Pokemon level based on Trainer level Match with an opaque shade of orange instead

--- app/src/main/java/com/kamron/pogoiv/Pokefly.java
+++ app/src/main/java/com/kamron/pogoiv/Pokefly.java
@@ -808,7 +808,8 @@ public class Pokefly extends Service {
      *
      */
     private void adjustSeekbarForPokemon() {
-        expandedLevelSeekbarBackground.getThumb(0).setInvisibleThumb(true);
+        expandedLevelSeekbarBackground.getThumb(0).setThumb(getResources().getDrawable(R.drawable
+                .orange_scrubber_control_disabled_holo));
         expandedLevelSeekbarBackground.getThumb(0).setValue(levelToSeekbarProgress(trainerLevel + 1.5f));
         expandedLevelSeekbarBackground.getThumb(1).setInvisibleThumb(true);
         expandedLevelSeekbarBackground.setMax(levelToSeekbarProgress(40));

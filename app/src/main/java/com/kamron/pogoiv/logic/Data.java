farkam135/GoIV

commit b2befba22d7b39c25cd2ad706bae633408abc713

Pokefly bugfix: reuse existing logic on levels
The bug here was for `estimatedPokemonLevel < 40.5`. Also, some of the
methods we're reusing might need to change.

app/src/main/java/com/kamron/pogoiv/logic/Data.java
@@ -70,7 +70,7 @@ public class Data {
      * and subtracting 2.
      */
     public static int levelToLevelIdx(double level) {
-        return (int) (level * 2) - 2;
+        return (int) (level - 1) * 2;
     }
 
     /**

app/src/main/java/com/kamron/pogoiv/Pokefly.java
@@ -658,22 +658,25 @@ public class Pokefly extends Service {
         toggleVisibility(inputAppraisalExpandBox, appraisalBox);
     }
 
+    public void adjustPokemonLevel(double estimatedPokemonLevel) {
+        setArcPointer(estimatedPokemonLevel);
+        arcAdjustBar.setProgress(Data.levelToLevelIdx(estimatedPokemonLevel));
+    }
+
     @OnClick(R.id.btnDecrementLevel)
     public void decrementLevel() {
         if (estimatedPokemonLevel > 1.0) {
             estimatedPokemonLevel -= 0.5;
         }
-        setArcPointer(estimatedPokemonLevel);
-        arcAdjustBar.setProgress((int) ((estimatedPokemonLevel - 1) * 2));
+        adjustPokemonLevel(estimatedPokemonLevel);
     }
 
     @OnClick(R.id.btnIncrementLevel)
     public void incrementLevel() {
-        if (estimatedPokemonLevel < trainerLevel + 1.5 && estimatedPokemonLevel < 40.5) {
+        if (estimatedPokemonLevel < Data.trainerLevelToMaxPokeLevel(trainerLevel)) {
             estimatedPokemonLevel += 0.5;
         }
-        setArcPointer(estimatedPokemonLevel);
-        arcAdjustBar.setProgress((int) ((estimatedPokemonLevel - 1) * 2));
+        adjustPokemonLevel(estimatedPokemonLevel);
     }
 
     @OnClick(R.id.btnIncrementLevelExpanded)

commit c03bb3962eab59e2d4a2332b24059cab3ec28fda

Fix level miscalculation
Regression from b2befba22d7b39c25cd2ad706bae633408abc713, where I made
the formula more "intuitive". The problem was the truncation before
multiplying.

app/src/main/java/com/kamron/pogoiv/logic/Data.java
@@ -71,7 +71,7 @@ public class Data {
      * and subtracting 2.
      */
     public static int levelToLevelIdx(double level) {
-        return (int) (level - 1) * 2;
+        return (int) ((level - 1) * 2);
     }
 
     /**
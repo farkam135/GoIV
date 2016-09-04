2234723be4f73c2a5b459686169ae5a260871619
nahojjjen <johansw95@gmail.com>
2016-09-04 15:15:21 +0200

    Adds a team picking spinner that saves its setting

--- app/src/main/java/com/kamron/pogoiv/GoIVSettings.java
+++ app/src/main/java/com/kamron/pogoiv/GoIVSettings.java
@@ -14,6 +14,7 @@ public class GoIVSettings {
     public static final String COPY_TO_CLIPBOARD = "copyToClipboard";
     public static final String SEND_CRASH_REPORTS = "sendCrashReports";
     public static final String AUTO_UPDATE_ENABLED = "autoUpdateEnabled";
+    public static final String TEAM_NAME = "teamName";
 
     private static GoIVSettings instance;
 
@@ -45,6 +46,13 @@ public class GoIVSettings {
         } else {
             return prefs.getBoolean(MANUAL_SCREENSHOT_MODE, false);
         }
+    }
+
+    public int playerTeam(){ return prefs.getInt(TEAM_NAME, -1);  }
+    public void setPlayerTeam(int value){
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(GoIVSettings.TEAM_NAME, value);
+        editor.commit();
     }
 
     public boolean shouldDeleteScreenshots() {

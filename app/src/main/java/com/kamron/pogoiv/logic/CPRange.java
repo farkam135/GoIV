35a282b8392b77ddbf1442f790d311c29d005c6e
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2016-09-04 19:03:55 +0200

    Drop unused CPRange members

--- app/src/main/java/com/kamron/pogoiv/logic/CPRange.java
+++ app/src/main/java/com/kamron/pogoiv/logic/CPRange.java
@@ -1,16 +1,15 @@
 package com.kamron.pogoiv.logic;
 
 /**
+ * Represent a range of CP.
  * Created by Johan on 2016-08-18.
  */
 public class CPRange {
-    public final int high;
     public final int low;
-    public final double level;
+    public final int high;
 
-    public CPRange(int high, int low, double level) {
+    public CPRange(int low, int high) {
         this.high = high;
         this.low = low;
-        this.level = level;
     }
 }

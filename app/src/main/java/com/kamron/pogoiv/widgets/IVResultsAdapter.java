70a507e9f0fdea93346d02da4708d05ea8b8448b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2016-09-11 01:03:29 +0200

    Fix compile
    I removed this import in the previous commit, but that's still needed in
    this branch.

--- app/src/main/java/com/kamron/pogoiv/widgets/IVResultsAdapter.java
+++ app/src/main/java/com/kamron/pogoiv/widgets/IVResultsAdapter.java
@@ -1,5 +1,6 @@
 package com.kamron.pogoiv.widgets;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;

11fbcfdb9662cd88ad4c25360055683bc6e91497
nahojjjen <johansw95@gmail.com>
2017-06-22 16:00:58 +0200

    Fixes prediction with appraisal constraints
    Because the IVScan object "high" and "low" instance variables were not
    updated when appraisal information was added, the predicted CP and such
    were not as correct as the could be, since they made the prediction as
    if there was no appraisal info added.

--- app/src/main/java/com/kamron/pogoiv/logic/IVScanResult.java
+++ app/src/main/java/com/kamron/pogoiv/logic/IVScanResult.java
@@ -157,6 +157,53 @@ public class IVScanResult {
         return new IVCombination(lowAttack, lowDefense, lowStamina);
     }
 
+
+    /**
+     * Readjusts the low and high instance variables by looping through all the combinations again and re-checking them.
+     */
+    private void updateHighAndLowValues() {
+        lowAttack = 15;
+        lowDefense = 15;
+        lowStamina = 15;
+        highAttack = 0;
+        highDefense = 0;
+        highStamina = 0;
+        highPercent = 0;
+        lowPercent = 100;
+
+        for (IVCombination ivc : iVCombinations) {
+            int sumIV = ivc.att + ivc.def + ivc.sta;
+            int percentPerfect = Math.round(sumIV / 45f * 100);
+
+            if (ivc.att > lowAttack) {
+                lowAttack = ivc.att;
+            }
+            if (ivc.def > lowDefense) {
+                lowDefense = ivc.def;
+            }
+            if (ivc.sta > lowStamina) {
+                lowStamina = ivc.sta;
+            }
+
+            if (ivc.att > highAttack) {
+                highAttack = ivc.att;
+            }
+            if (ivc.def > highDefense) {
+                highDefense = ivc.def;
+            }
+            if (ivc.sta > highStamina) {
+                highStamina = ivc.sta;
+            }
+            if (percentPerfect > highPercent) {
+                highPercent = percentPerfect;
+            }
+            if (percentPerfect < lowPercent) {
+                lowPercent = percentPerfect;
+            }
+        }
+
+    }
+
     /**
      * Removes all possible IV combinations where the boolean set to true stat isnt the highest.
      * Several stats can be highest if they're equal.
@@ -171,6 +218,7 @@ public class IVScanResult {
             }
         }
         iVCombinations = refinedList;
+        updateHighAndLowValues();
     }
 
     /**
@@ -218,6 +266,7 @@ public class IVScanResult {
         }
 
         iVCombinations = refinedList;
+        updateHighAndLowValues();
     }
 
     /**
@@ -265,5 +314,7 @@ public class IVScanResult {
             }
         }
         iVCombinations = refinedList;
+
+        updateHighAndLowValues();
     }
 }

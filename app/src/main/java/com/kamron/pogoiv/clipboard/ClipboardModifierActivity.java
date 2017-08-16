6bd2ea611ca8b672931547d755abfb04a7404721
Mattia Aracne <mattia@codeatlas.it>
2017-08-17 01:19:36 +0200

    Final touches to the new clipboard UI
    Refactor out some UI logic from ClipboardModifierActivity to
    ClipboardModifierFragment.
    
    Add a ViewPager to switch between single and multiple result edit mode
    with a swipe from right or left.
    
    Add two mini floating action buttons to control max evolution variants
    and token add action.
    
    Move save icon to the ActionBar and show home as up.
    
    Tweak tokens layout colors to make everything more readable.

--- app/src/main/java/com/kamron/pogoiv/clipboard/ClipboardModifierActivity.java
+++ app/src/main/java/com/kamron/pogoiv/clipboard/ClipboardModifierActivity.java
@@ -1,246 +1,149 @@
 package com.kamron.pogoiv.clipboard;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.common.base.Strings;
 import com.kamron.pogoiv.GoIVSettings;
 import com.kamron.pogoiv.R;
-import com.kamron.pogoiv.clipboard.adapters.TokensPreviewAdapter;
-import com.kamron.pogoiv.clipboard.adapters.TokensShowcaseAdapter;
-import com.kamron.pogoiv.clipboard.layoutmanagers.TokenGridLayoutManager;
-import com.kamron.pogoiv.clipboard.tokens.CustomSeparatorToken;
-import com.kamron.pogoiv.clipboard.tokens.SeparatorToken;
 
-import java.util.List;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class ClipboardModifierActivity
-        extends AppCompatActivity
-        implements ClipboardToken.OnTokenSelectedListener, ClipboardToken.OnTokenDeleteListener {
+public class ClipboardModifierActivity extends AppCompatActivity {
+
+    @BindView(R.id.pager)
+    ViewPager viewPager;
+    @BindView(R.id.pagerTabStrip)
+    PagerTabStrip pagerTabStrip;
+    @BindView(R.id.clipboardDescription)
+    TextView clipboardDescription;
 
     private ClipboardTokenHandler cth;
-    private TextView clipboardMaxLength;
-    private Spinner resultModeSpinner;
-    private TextView clipboardDescription;
-    private CheckBox clipboardMaxEvolutionVariant;
-    private LinearLayout singleMultiLayout;
-
-    private TokensPreviewAdapter tokenPreviewAdapter;
-    private TokensShowcaseAdapter tokenShowcaseAdapter;
-    private ClipboardToken selectedToken = null;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setTitle(R.string.clipboard_activity_title);
         setContentView(R.layout.activity_clipboard_modifier);
-        initiateInstanceVariables();
-        setSingleCheckboxShownDependingOnSetting(); //must be done after initiateInstanceVariables
-        updateFields();
+        ButterKnife.bind(this);
 
-        // Init the spinner that let the user switch from multiple result to single result edit mode.
-        resultModeSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
-                new String[] {"Multiple result", "Single result"}));
-        resultModeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                updateFields();
+        cth = new ClipboardTokenHandler(this);
+
+        boolean singleModeEnabled = isSingleModeEnabled();
+        ModePagerAdapter pagerAdapter = new ModePagerAdapter(getSupportFragmentManager(), singleModeEnabled);
+        viewPager.setAdapter(pagerAdapter);
+
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.clipboard_activity_title);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+            if (!singleModeEnabled) {
+                pagerTabStrip.setVisibility(View.GONE);
+            } else {
+                actionBar.setElevation(0);
+                pagerTabStrip.setTabIndicatorColor(ContextCompat.getColor(this, R.color.colorAccent));
             }
-
-            @Override public void onNothingSelected(AdapterView<?> parent) {
-                // Nothing to do
-            }
-        });
-    }
-
-    /**
-     * Hides the checkfox for choosing to edit the single or multiple clipboard results.
-     */
-    private void setSingleCheckboxShownDependingOnSetting() {
-        GoIVSettings settings = GoIVSettings.getInstance(this);
-        if (settings.shouldCopyToClipboardSingle()) {
-            singleMultiLayout.setVisibility(View.VISIBLE);
-        } else {
-            singleMultiLayout.setVisibility(View.GONE);
         }
     }
 
-    /**
-     * Initiates instance variables.
-     */
-    private void initiateInstanceVariables() {
-        cth = new ClipboardTokenHandler(this);
-        resultModeSpinner = (Spinner) findViewById(R.id.resultModeSpinner);
-        clipboardMaxLength = (TextView) findViewById(R.id.clipboardMaxLength);
-        clipboardDescription = (TextView) findViewById(R.id.clipboardDescription);
-        RecyclerView tokenPreviewRecyclerView = (RecyclerView) findViewById(R.id.tokenPreviewRecyclerView);
-        clipboardMaxEvolutionVariant = (CheckBox) findViewById(R.id.clipboardMaxEvolutionVariant);
-        singleMultiLayout = (LinearLayout) findViewById(R.id.singleMultiLayout);
-        RecyclerView tokenShowcaseRecyclerView = (RecyclerView) findViewById(R.id.tokenShowcaseRecyclerView);
-
-        // Populate the token preview RecyclerView with all configured tokens.
-        tokenPreviewAdapter = new TokensPreviewAdapter(getCurrentlyModifyingList(), this);
-        tokenPreviewRecyclerView.setHasFixedSize(false);
-        tokenPreviewRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        tokenPreviewRecyclerView.setAdapter(tokenPreviewAdapter);
-
-        // Populate the token showcase RecyclerView with all possible tokens. The TokenListAdapter will put them in
-        // their respective category while TokenGridLayoutManager will arrange them in a grid with category headers
-        // that span the entire RecyclerView width.
-        tokenShowcaseAdapter = new TokensShowcaseAdapter(this, clipboardMaxEvolutionVariant.isChecked(), this);
-        tokenShowcaseRecyclerView.setHasFixedSize(false);
-        tokenShowcaseRecyclerView.setLayoutManager(new TokenGridLayoutManager(this, tokenShowcaseAdapter));
-        tokenShowcaseRecyclerView.setAdapter(tokenShowcaseAdapter);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.clipboard_menu, menu);
+        return true;
     }
 
-    private boolean isSingleResultMode() {
-        return resultModeSpinner.getSelectedItemPosition() == 1;
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            case R.id.save:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
-    /**
-     * Get a list of tokens, the default list if user is currently modifying the default list, or the single result
-     * user token list if that checkbox is marked.
-     *
-     * @return The users token setting for either single or multiple results
-     */
-    private List<ClipboardToken> getCurrentlyModifyingList() {
-        return cth.getTokens(isSingleResultMode());
+    private boolean isSingleModeEnabled() {
+        GoIVSettings settings = GoIVSettings.getInstance(this);
+        return settings.shouldCopyToClipboardSingle();
+    }
+
+    public ClipboardTokenHandler getClipboardTokenHandler() {
+        return cth;
+    }
+
+    @Override public void finish() {
+        super.finish();
+        Toast.makeText(this, "Configuration saved!", Toast.LENGTH_SHORT).show();
     }
 
     /**
-     * Toggles showing the evolved variants of tokens in the list.
-     *
-     * @param v needed for xml onclick.
+     * Sets the description for the selected token.
      */
-    public void toggleEvolvedVariant(View v) {
-        tokenShowcaseAdapter.setEvolvedVariant(clipboardMaxEvolutionVariant.isChecked());
-    }
-
-    /**
-     * Select a token to show its description.
-     *
-     * @param token Which token to show.
-     */
-    @Override public void onTokenSelected(ClipboardToken token, int adapterPosition) {
-        selectedToken = token;
-        updateClipboardDescription();
-        updateLengthIndicator();
-    }
-
-    @Override public void onTokenDeleted(int adapterPosition) {
-        cth.removeToken(adapterPosition, isSingleResultMode());
-        updateFields();
-    }
-
-    /**
-     * Updates the description, the preview window, the highlighted single/multi text,  and the editor window.
-     */
-    private void updateFields() {
-        updateClipboardDescription();
-        updateLengthIndicator();
-        tokenPreviewAdapter.setData(getCurrentlyModifyingList());
-    }
-
-    /**
-     * Sets the clipboard description for the selected token.
-     */
-    private void updateClipboardDescription() {
-
+    public void updateTokenDescription(ClipboardToken selectedToken) {
         if (selectedToken == null) {
             clipboardDescription.setText("No token selected...");
         } else if (selectedToken.maxEv) {
-            clipboardDescription.setText(selectedToken.getLongDescription(this) + " This token is a max evolution "
+            clipboardDescription.setText(selectedToken.getLongDescription(this) + "\n\nThis token is a max evolution "
                     + "variant, meaning that it will return a result as if your monster was already fully evolved, "
                     + "which might be more interesting in a lot of cases.");
         } else { //selectedtoken not max ev
             clipboardDescription.setText(selectedToken.getLongDescription(this));
         }
-
     }
 
-    /**
-     * Add a token to the user settings.
-     *
-     * @param v needed for onclick xml.
-     */
-    public void addToken(View v) {
-        if (selectedToken != null) {
-            if (selectedToken instanceof CustomSeparatorToken) {
-                buildCustomSeparatorToken();
-            } else {
-                cth.addToken(selectedToken, isSingleResultMode());
-                tokenPreviewAdapter.setData(getCurrentlyModifyingList());
-                updateLengthIndicator();
-            }
-        } else {
-            Toast.makeText(this, R.string.clipboard_no_token_selected, Toast.LENGTH_LONG).show();
+    private static class ModePagerAdapter extends FragmentPagerAdapter {
+
+        final boolean singleResultModeEnabled;
+
+        public ModePagerAdapter(FragmentManager fm, boolean singleResultModeEnabled) {
+            super(fm);
+            this.singleResultModeEnabled = singleResultModeEnabled;
         }
-    }
 
-    /**
-     * Builds a Dialog to create a custom string token to be added to the user settings.
-     */
-    public void buildCustomSeparatorToken() {
-        // The custom separator will be written in this EditText
-        final View dialogView = LayoutInflater.from(this).inflate(R.layout.edittext_dialog, null);
-        final EditText editText = (EditText) dialogView.findViewById(R.id.editText);
+        @Override
+        public int getCount() {
+            return singleResultModeEnabled ? 2 : 1;
+        }
 
-        // This dialog will implement the user interaction
-        new AlertDialog.Builder(this)
-                .setView(dialogView)
-                .setMessage("Please input your custom separator")
-                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override public void onClick(DialogInterface dialogInterface, int i) {
-                        String separator = editText.getText().toString().trim();
-                        if (Strings.isNullOrEmpty(separator)) {
-                            Toast.makeText(ClipboardModifierActivity.this,
-                                    "Please fill in your custom separator", Toast.LENGTH_LONG).show();
-                            return;
-                        }
-                        if (separator.contains(".")) {
-                            Toast.makeText(ClipboardModifierActivity.this, "Custom separator can't contain ."
-                                    + " because the developer is lazy", Toast.LENGTH_LONG).show();
-                        }
-                        selectedToken = new SeparatorToken(separator);
-                        addToken(null);
-                    }
-                })
-                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-                    @Override public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.cancel();
-                    }
-                })
-                .show();
-    }
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return ClipboardModifierFragment.newInstance(false);
+                case 1:
+                    return ClipboardModifierFragment.newInstance(true);
+                default:
+                    return null;
+            }
+        }
 
-    /**
-     * Finishes and exits the activity.
-     *
-     * @param v needed for onclick xml.
-     */
-    public void saveAndExit(View v) {
-        finish();
-    }
-
-    /**
-     * Updates the preview length indicator.
-     */
-    public void updateLengthIndicator() {
-        clipboardMaxLength.setText("(" + cth.getMaxLength(isSingleResultMode()) + "/12 characters)");
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Multiple result";
+                case 1:
+                    return "Single result";
+                default:
+                    return null;
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/com/kamron/pogoiv/clipboard/ClipboardModifierFragment.java
@@ -0,0 +1,251 @@
+package com.kamron.pogoiv.clipboard;
+
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.common.base.Strings;
+import com.kamron.pogoiv.R;
+import com.kamron.pogoiv.clipboard.adapters.TokensPreviewAdapter;
+import com.kamron.pogoiv.clipboard.adapters.TokensShowcaseAdapter;
+import com.kamron.pogoiv.clipboard.layoutmanagers.TokenGridLayoutManager;
+import com.kamron.pogoiv.clipboard.tokens.CustomSeparatorToken;
+import com.kamron.pogoiv.clipboard.tokens.SeparatorToken;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static android.support.v7.widget.LinearLayoutManager.HORIZONTAL;
+
+
+public class ClipboardModifierFragment
+        extends Fragment
+        implements ClipboardToken.OnTokenSelectedListener, ClipboardToken.OnTokenDeleteListener {
+
+    private static final String ARG_SINGLE_RESULT_MODE = "a_srm";
+
+
+    @BindView(R.id.clipboardMaxLength)
+    TextView clipboardMaxLength;
+    @BindView(R.id.tokenPreviewRecyclerView)
+    RecyclerView tokenPreviewRecyclerView;
+    @BindView(R.id.tokenShowcaseRecyclerView)
+    RecyclerView tokenShowcaseRecyclerView;
+    @BindView(R.id.btnAdd)
+    FloatingActionButton btnAdd;
+    @BindView(R.id.btnMaxEvolution)
+    FloatingActionButton btnMaxEvolution;
+
+
+    private boolean singleResultMode;
+    private boolean maxEvolutionVariant = true;
+    private TokensPreviewAdapter tokenPreviewAdapter;
+    private TokensShowcaseAdapter tokenShowcaseAdapter;
+    private ClipboardToken selectedToken = null;
+
+
+    public static ClipboardModifierFragment newInstance(boolean singleResultMode) {
+        Bundle args = new Bundle();
+        args.putBoolean(ARG_SINGLE_RESULT_MODE, singleResultMode);
+        ClipboardModifierFragment f = new ClipboardModifierFragment();
+        f.setArguments(args);
+        return f;
+    }
+
+    public ClipboardModifierFragment() {
+        super();
+    }
+
+    @Override public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        singleResultMode = getArguments().getBoolean(ARG_SINGLE_RESULT_MODE, false);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_clipboard_modifier, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Populate the token preview RecyclerView with all configured tokens.
+        tokenPreviewAdapter = new TokensPreviewAdapter(getCurrentlyModifyingList(), this);
+        tokenPreviewRecyclerView.setHasFixedSize(false);
+        tokenPreviewRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), HORIZONTAL, false));
+        tokenPreviewRecyclerView.setAdapter(tokenPreviewAdapter);
+
+        // Populate the token showcase RecyclerView with all possible tokens. The TokenListAdapter will put them in
+        // their respective category while TokenGridLayoutManager will arrange them in a grid with category headers
+        // that span the entire RecyclerView width.
+        tokenShowcaseAdapter = new TokensShowcaseAdapter(getContext(), maxEvolutionVariant, this);
+        tokenShowcaseRecyclerView.setHasFixedSize(false);
+        tokenShowcaseRecyclerView.setLayoutManager(new TokenGridLayoutManager(getContext(), tokenShowcaseAdapter));
+        tokenShowcaseRecyclerView.setAdapter(tokenShowcaseAdapter);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                addToken();
+            }
+        });
+
+        btnMaxEvolution.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                maxEvolutionVariant = !maxEvolutionVariant;
+                if (maxEvolutionVariant) {
+                    btnMaxEvolution.setImageDrawable(
+                            ContextCompat.getDrawable(getContext(), R.drawable.ic_star_white_18dp));
+                    Toast.makeText(getContext(), "Show tokens max evolution variant", Toast.LENGTH_SHORT).show();
+                } else {
+                    btnMaxEvolution.setImageDrawable(
+                            ContextCompat.getDrawable(getContext(), R.drawable.ic_star_border_white_18dp));
+                    Toast.makeText(getContext(), "Show standard tokens", Toast.LENGTH_SHORT).show();
+                }
+                tokenShowcaseAdapter.setEvolvedVariant(maxEvolutionVariant);
+            }
+        });
+    }
+
+    @Override public void onResume() {
+        super.onResume();
+        updateFields();
+    }
+
+    /**
+     * Select a token to show its description.
+     *
+     * @param token Which token to show.
+     */
+    @Override public void onTokenSelected(ClipboardToken token, int adapterPosition) {
+        selectedToken = token;
+        updateClipboardDescription();
+        updateLengthIndicator();
+    }
+
+    @Override public void onTokenDeleted(int adapterPosition) {
+        getClipboardTokenHandler().removeToken(adapterPosition, singleResultMode);
+        updateFields();
+    }
+
+    private @NonNull ClipboardTokenHandler getClipboardTokenHandler() {
+        final Activity activity = getActivity();
+        if (activity instanceof ClipboardModifierActivity) {
+            return ((ClipboardModifierActivity) activity).getClipboardTokenHandler();
+        }
+        throw new IllegalStateException();
+    }
+
+    /**
+     * Get a list of tokens, the default list if user is currently modifying the default list, or the single result
+     * user token list if that checkbox is marked.
+     *
+     * @return The users token setting for either single or multiple results
+     */
+    private List<ClipboardToken> getCurrentlyModifyingList() {
+        return getClipboardTokenHandler().getTokens(singleResultMode);
+    }
+
+    /**
+     * Add a token to the user settings.
+     */
+    public void addToken() {
+        if (selectedToken != null) {
+            if (selectedToken instanceof CustomSeparatorToken) {
+                buildCustomSeparatorToken();
+            } else {
+                getClipboardTokenHandler().addToken(selectedToken, singleResultMode);
+                tokenPreviewAdapter.setData(getCurrentlyModifyingList());
+                tokenPreviewRecyclerView.smoothScrollToPosition(tokenPreviewAdapter.getItemCount() - 1);
+                updateLengthIndicator();
+            }
+        } else {
+            Toast.makeText(getContext(), R.string.clipboard_no_token_selected, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void updateClipboardDescription() {
+        final Activity activity = getActivity();
+        if (activity instanceof ClipboardModifierActivity) {
+            ((ClipboardModifierActivity) activity).updateTokenDescription(selectedToken);
+        }
+    }
+
+    /**
+     * Updates the preview length indicator.
+     */
+    @SuppressLint("DefaultLocale")
+    private void updateLengthIndicator() {
+        final Activity activity = getActivity();
+        if (activity instanceof ClipboardModifierActivity) {
+            clipboardMaxLength.setText(String.format("(%1$d/12 characters)",
+                    getClipboardTokenHandler().getMaxLength(singleResultMode)));
+        }
+    }
+
+    /**
+     * Builds a Dialog to create a custom string token to be added to the user settings.
+     */
+    public void buildCustomSeparatorToken() {
+        // The custom separator will be written in this EditText
+        final View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.edittext_dialog, null);
+        final EditText editText = (EditText) dialogView.findViewById(R.id.editText);
+
+        // This dialog will implement the user interaction
+        new AlertDialog.Builder(getContext())
+                .setView(dialogView)
+                .setMessage("Please input your custom separator")
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override public void onClick(DialogInterface dialogInterface, int i) {
+                        String separator = editText.getText().toString().trim();
+                        if (Strings.isNullOrEmpty(separator)) {
+                            Toast.makeText(getContext(),
+                                    "Please fill in your custom separator", Toast.LENGTH_LONG).show();
+                            return;
+                        }
+                        if (separator.contains(".")) {
+                            Toast.makeText(getContext(), "Custom separator can't contain ."
+                                    + " because the developer is lazy", Toast.LENGTH_LONG).show();
+                        }
+                        selectedToken = new SeparatorToken(separator);
+                        addToken();
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                })
+                .show();
+    }
+
+    /**
+     * Updates the description, the preview window, the highlighted single/multi text,  and the editor window.
+     */
+    private void updateFields() {
+        // TODO updateClipboardDescription();
+        updateLengthIndicator();
+        tokenPreviewAdapter.setData(getCurrentlyModifyingList());
+    }
+
+}

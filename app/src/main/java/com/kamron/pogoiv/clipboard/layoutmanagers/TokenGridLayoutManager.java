ff8f66657ebb8fe2d093661be5bfd8406ad12a30
Mattia Aracne <thearaks@gmail.com>
2017-08-19 13:29:54 +0200

    Improve MarginItemDecorator behavior

--- app/src/main/java/com/kamron/pogoiv/clipboard/layoutmanagers/TokenGridLayoutManager.java
+++ app/src/main/java/com/kamron/pogoiv/clipboard/layoutmanagers/TokenGridLayoutManager.java
@@ -10,20 +10,29 @@ public class TokenGridLayoutManager extends GridLayoutManager {
 
     private static int SPAN_COUNT = 3;
 
-    public TokenGridLayoutManager(Context context, final TokensShowcaseAdapter tokensShowcaseAdapter) {
+    public TokenGridLayoutManager(Context context, TokensShowcaseAdapter tokensShowcaseAdapter) {
         super(context, SPAN_COUNT);
-        setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
-            @Override
-            public int getSpanSize(int position) {
-                switch (tokensShowcaseAdapter.getItemViewType(position)) {
-                    case TokensShowcaseAdapter.VIEW_TYPE_HEADER:
-                        return SPAN_COUNT;
-                    case TokensShowcaseAdapter.VIEW_TYPE_TOKEN:
-                        return 1;
-                    default:
-                        throw new IllegalArgumentException();
-                }
+        setSpanSizeLookup(new TokenSpanSizeLookup(tokensShowcaseAdapter));
+    }
+
+    private static class TokenSpanSizeLookup extends SpanSizeLookup {
+        TokensShowcaseAdapter adapter;
+
+        TokenSpanSizeLookup(TokensShowcaseAdapter adapter) {
+            this.adapter = adapter;
+            setSpanIndexCacheEnabled(true);
+        }
+
+        @Override
+        public int getSpanSize(int position) {
+            switch (adapter.getItemViewType(position)) {
+                case TokensShowcaseAdapter.VIEW_TYPE_HEADER:
+                    return SPAN_COUNT;
+                case TokensShowcaseAdapter.VIEW_TYPE_TOKEN:
+                    return 1;
+                default:
+                    throw new IllegalArgumentException();
             }
-        });
+        }
     }
 }

6edcbe5cc43df24f0b85fbb23005883db5a1ca60
MARTINI1 <martini25onepiece@gmail.com>
2017-08-29 18:45:50 +0200

    More hardcode text to strings.xml

--- app/src/main/java/com/kamron/pogoiv/pokeflycomponents/IVPreviewPrinter.java
+++ app/src/main/java/com/kamron/pogoiv/pokeflycomponents/IVPreviewPrinter.java
@@ -65,7 +65,7 @@ public class IVPreviewPrinter {
         public void run() {
             boolean succeeded = runQuickScan();
             if (!succeeded) {
-                Toast.makeText(pokefly, "Touch screen to retry quick IV scan", Toast.LENGTH_SHORT).show();
+                Toast.makeText(pokefly, R.string.touch_screen_quick_iv_scan, Toast.LENGTH_SHORT).show();
             }
         }
 

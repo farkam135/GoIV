c579440bec00c0a1b38cd145a533f8c08119cf71
nahojjjen <johansw95@gmail.com>
2017-10-01 15:50:39 +0200

    Removes unused import

--- app/src/main/java/com/kamron/pogoiv/activities/OcrCalibrationResultActivity.java
+++ app/src/main/java/com/kamron/pogoiv/activities/OcrCalibrationResultActivity.java
@@ -13,7 +13,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.provider.MediaStore;
 import android.support.annotation.ColorInt;
 import android.support.annotation.MainThread;
 import android.support.v4.content.ContextCompat;

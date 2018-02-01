6b6897f1d3a85ac9094afe547ae2e837bec49cf4
udnp <udnp@sawdagger.nagoya>
2018-02-01 21:17:47 +0900

    [C] changed to show pokedex number in the IVPopupButton scan result.

--- app/src/main/java/com/kamron/pogoiv/pokeflycomponents/IVPopupButton.java
+++ app/src/main/java/com/kamron/pogoiv/pokeflycomponents/IVPopupButton.java
@@ -107,9 +107,9 @@ public class IVPopupButton extends android.support.v7.widget.AppCompatButton {
         int low = ivrs.getLowestIVCombination().percentPerfect;
         int high = ivrs.getHighestIVCombination().percentPerfect;
         if (ivrs.getCount() == 1 || high == low) { // display something like "IV: 98%"
-            setText(ivrs.pokemon.name + "\nIV: " + low + "%");
+            setText("#" + (ivrs.pokemon.number + 1) + " " + ivrs.pokemon.name + "\nIV: " + low + "%");
         } else { // display something like "IV: 55 - 87%"
-            setText(ivrs.pokemon.name + "\nIV: " + low + " - " + high + "%");
+            setText("#" + (ivrs.pokemon.number + 1) + " " + ivrs.pokemon.name + "\nIV: " + low + " - " + high + "%");
         }
         if (ivrs.rangeIVScan) {
             setText(getText() + "*");

43f7840bdd45db70670988cee61971ae6d21eed1
Ryan Barker <celandro@gmail.com>
2018-06-14 23:21:18 -0700

    Update for pokebattler data

--- app/src/test/java/com/kamron/pogoiv/thirdparty/MovesetFetchUtilTest.java
+++ app/src/test/java/com/kamron/pogoiv/thirdparty/MovesetFetchUtilTest.java
@@ -23,48 +23,70 @@ import okhttp3.Response;
 import timber.log.Timber;
 
 
-
+/**
+ * A class for generating the moveset database json by querying pokebattler.
+ * Run the generateMovesetList test to update the json in
+ * app/src/main/assets/thirdparty/pokebattler//pokemonMovesetData.json.
+ */
 public class MovesetFetchUtilTest {
-//    private static final String BASE_URL = "https://fight.pokebattler.com" ;
-//    private static final String BASE_URL = "http://localhost:8001" ;
-    private static final String BASE_URL = "https://20180530t224949-dot-fight-dot-pokebattler-1380.appspot.com" ;
+    private static final String BASE_URL = "https://fight.pokebattler.com";
+//    private static final String BASE_URL = "http://localhost:8001";
+//    private static final String BASE_URL = "https://20180530t224949-dot-fight-dot-pokebattler-1380.appspot.com";
+    OkHttpClient httpClient = new OkHttpClient();
+    ;
 
-    OkHttpClient httpClient;
-
+    /**
+     * This "test" generates a json of all pokemon move ratings by querying the pokebattler database.
+     */
     @Test
-    public void thisShouldBeMain() throws Exception{
+    public void generateMovesetDatabase() throws Exception {
 //        Timber.plant(new Timber.DebugTree()); This throws exceptions in unit tests
         MovesetFetchUtilTest fetcher = new MovesetFetchUtilTest();
         Map<String, List<MovesetData>> pokemon = fetcher.fetchAllPokemon();
         JSONObject toDump = new JSONObject(pokemon);
         try (FileWriter writer = new FileWriter(new File
                 ("app/src/main/assets/thirdparty/pokebattler//pokemonMovesetData.json")
+
         )) {
             writer.write(toDump.toString());
+            System.out.println(toDump.toString());
         }
 
     }
 
-    public MovesetFetchUtilTest() {
-        httpClient = new OkHttpClient();
-    }
     public String getAttackURL(String pokemon) {
-        return BASE_URL + "/rankings/attackers/levels/30/defenders/levels/30/strategies/CINEMATIC_ATTACK_WHEN_POSSIBLE/DEFENSE_RANDOM_MC"
-                + "?sort=OVERALL&dodgeStrategy=DODGE_REACTION_TIME&weatherCondition=NO_WEATHER&filterType=TOP_DEFENDER&filterValue="
+        return BASE_URL
+                + "/rankings/attackers/levels/30/defenders/levels/30/strategies/CINEMATIC_ATTACK_WHEN_POSSIBLE"
+                + "/DEFENSE_RANDOM_MC"
+                + "?sort=OVERALL&dodgeStrategy=DODGE_REACTION_TIME&weatherCondition=NO_WEATHER&filterType"
+                + "=TOP_DEFENDER&filterValue="
                 + pokemon;
     }
+
     public String getDefenseURL(String pokemon) {
-        return BASE_URL + "/rankings/defenders/levels/30/attackers/levels/30/strategies/DEFENSE_RANDOM_MC/CINEMATIC_ATTACK_WHEN_POSSIBLE"
-                + "?sort=OVERALL&dodgeStrategy=DODGE_REACTION_TIME&weatherCondition=NO_WEATHER&filterType=POKEMON&filterValue="
+        return BASE_URL
+                + "/rankings/defenders/levels/30/attackers/levels/30/strategies/DEFENSE_RANDOM_MC"
+                + "/CINEMATIC_ATTACK_WHEN_POSSIBLE"
+                + "?sort=OVERALL&dodgeStrategy=DODGE_REACTION_TIME&weatherCondition=NO_WEATHER&filterType=POKEMON"
+                + "&filterValue="
                 + pokemon;
     }
+
+
+    /**
+     * Get a list of all attack and defence ratings for all movesets for all pokemon from an online database.
+     *
+     * @return A map, which has the pokemon names as keys, and a list of movesetdata as values.
+     */
     public Map<String, List<MovesetData>> fetchAllPokemon() {
         Map<String, List<MovesetData>> allPokemon = new TreeMap<>();
-        for (PokemonId pokemon: PokemonId.values()) {
-            if (pokemon == PokemonId.MISSINGNO || pokemon == PokemonId.UNRECOGNIZED) continue;
+        for (PokemonId pokemon : PokemonId.values()) {
+            if (pokemon == PokemonId.MISSINGNO || pokemon == PokemonId.UNRECOGNIZED) {
+                continue;
+            }
 
             try {
-                allPokemon.put(pokemon.name(), fetchPokemon(pokemon.name()));
+                allPokemon.put(pokemon.name(), fetchPokemonOnlineMovesets(pokemon.name()));
                 Timber.i("Finished fetching %s", pokemon.name());
                 //FIXME: The above doesnt properly log in unit tests
                 System.out.println("Finished fetching " + pokemon.name());
@@ -77,45 +99,57 @@ public class MovesetFetchUtilTest {
         return allPokemon;
     }
 
-
-    public List<MovesetData> fetchPokemon(String pokemon) {
-
-        TreeMap<MovesetData.Key,Double> attackScores =fetchScoreMap(getAttackURL(pokemon));
+    /**
+     * Get the moveset evaluation from online for a specific pokemon.
+     *
+     * @param pokemon The pokemon to search for.
+     * @return A list of moveset data for that specific pokemon.
+     */
+    public List<MovesetData> fetchPokemonOnlineMovesets(String pokemon) {
+        TreeMap<MovesetData.Key, Double> attackScores = fetchPokemonScoreMap(getAttackURL(pokemon));
         if (attackScores == null) {
             System.err.println("Unexpected null attack scores for " + pokemon);
             return Collections.emptyList();
         }
-        TreeMap<MovesetData.Key,Double> defenseScores =fetchScoreMap(getDefenseURL(pokemon));
+        TreeMap<MovesetData.Key, Double> defenseScores = fetchPokemonScoreMap(getDefenseURL(pokemon));
         if (defenseScores == null) {
             System.err.println("Unexpected null defense scores for " + pokemon);
             return Collections.emptyList();
         }
         List<MovesetData> retval = new ArrayList<>(attackScores.size());
         // add all the good attack scores first
-        for (Map.Entry<MovesetData.Key, Double> attackScoreEntry: attackScores.entrySet()) {
+        for (Map.Entry<MovesetData.Key, Double> attackScoreEntry : attackScores.entrySet()) {
             MovesetData.Key key = attackScoreEntry.getKey();
             Double defenseScore = defenseScores.get(key);
             //TODO merge with https://fight.pokebattler.com/pokemon and https://fight.pokebattler.com/moves
             retval.add(new MovesetData(key.getQuick(), key.getCharge(), false, false, attackScoreEntry.getValue(),
-            defenseScore, "UNKNOWN", "UNKNOWN"));
+                    defenseScore, "UNKNOWN", "UNKNOWN"));
         }
         // then add moves that are only good on defense
-        for (Map.Entry<MovesetData.Key, Double> defenseScoreEntry: defenseScores.entrySet()) {
+        for (Map.Entry<MovesetData.Key, Double> defenseScoreEntry : defenseScores.entrySet()) {
             MovesetData.Key key = defenseScoreEntry.getKey();
-            if (attackScores.containsKey(key))
+            if (attackScores.containsKey(key)) {
                 continue;
+            }
             retval.add(new MovesetData(key.getQuick(), key.getCharge(), false, false, null,
                     defenseScoreEntry.getValue(), "UNKNOWN", "UNKNOWN"));
         }
         return retval;
     }
 
-    private TreeMap<MovesetData.Key, Double> fetchScoreMap(String url) {
+    /**
+     * Get the score for all movesets, in either defence or attack score from an online database.
+     *
+     * @param url Either the attackURL or defenceURL for a pokemon.
+     * @return A treemap containing all possible movesets as keys, and the moveset score as value.
+     */
+    private TreeMap<MovesetData.Key, Double> fetchPokemonScoreMap(String url) {
         TreeMap<MovesetData.Key, Double> scores;
         Request request = new Request.Builder().url(url).build();
         try (Response response = httpClient.newCall(request).execute()) {
-            scores = getScoreMap(response);
-        } catch(Exception e) {
+            JSONObject pokemonInfo = new JSONObject(response.body().string());
+            scores = parseMovesetJson(pokemonInfo);
+        } catch (Exception e) {
             Timber.e("Could not fetch file");
             Timber.e(e);
             return new TreeMap<>();
@@ -123,17 +157,24 @@ public class MovesetFetchUtilTest {
         return scores;
     }
 
-    private TreeMap<MovesetData.Key,Double> getScoreMap( Response response) throws IOException {
+    /**
+     * Interprets the json response from the Pokebattler server into a treemap where each key is a moveset
+     * combination, and the value is the score of the moveset.
+     *
+     * @param jsonResponse The json response from pokebattler.
+     * @return A treemap with moveset keys and score values.
+     * @throws IOException
+     */
+    private TreeMap<MovesetData.Key, Double> parseMovesetJson(JSONObject jsonResponse) throws IOException {
         TreeMap<MovesetData.Key, Double> scores = new TreeMap<>();
         try {
-            JSONObject pokemonInfo = new JSONObject(response.body().string());
-            JSONArray moveRankings = pokemonInfo.getJSONArray("attackers").getJSONObject(0).getJSONArray("byMove");
+            JSONArray moveRankings = jsonResponse.getJSONArray("attackers").getJSONObject(0).getJSONArray("byMove");
             double maxScore = moveRankings.getJSONObject(0).getJSONObject("total").getDouble("overallRating");
-            for (int i=0; i<moveRankings.length(); i++) {
+            for (int i = 0; i < moveRankings.length(); i++) {
                 JSONObject move = moveRankings.getJSONObject(i);
                 double score = move.getJSONObject("total").getDouble("overallRating") / maxScore;
                 MovesetData.Key key = new MovesetData.Key(move.getString("move1"), move.getString("move2"));
-                scores.put(key,score);
+                scores.put(key, score);
             }
         } catch (JSONException je) {
             Timber.e("Exception thrown while checking for update");

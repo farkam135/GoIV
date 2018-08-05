92dc532b01fdb1d8a9a64d2b4e5239b86ce6e755
udnp <udnp@sawdagger.nagoya>
2018-08-05 17:13:08 +0900

    [Rf] added comment to describe it.

--- app/src/main/java/com/kamron/pogoiv/utils/StringUtils.java
+++ app/src/main/java/com/kamron/pogoiv/utils/StringUtils.java
@@ -4,9 +4,20 @@ import java.text.Normalizer;
 import java.util.Locale;
 
 public class StringUtils {
-/**
- * Returns the normalized string for simplifying words to detect pokemons.
- */
+
+    /**
+     * Returns the normalized string for simplifying characters to detect pokemons.
+     *
+     * Actually scanned character result quality is often not enough in some reasons like following:
+     *  - device screen resolution
+     *  - font properties(size, forms and ligature)
+     *  - letter complexity in some locales
+     *
+     * This method provides normalized string to simplify characters.
+     * For examples, special characters such as â, é, ば etc are replaced with their normalized forms a, e, は etc.
+     * So they can be compared more simply.
+     *
+     */
     public static String normalize(String s) {
         s = s.toLowerCase();
         s = Normalizer.normalize(s, Normalizer.Form.NFD);
@@ -16,6 +27,7 @@ public class StringUtils {
         //String lang = Locale.getDefault().getLanguage();
         //
         //if (Locale.getDefault().getLanguage().contains("ja")) {
+        //  ...
         //}
 
         return s;

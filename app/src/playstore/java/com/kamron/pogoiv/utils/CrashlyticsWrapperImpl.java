a3934029fe1763f445040bde3d2759699c5119c4
Timo Etzold <26691627+timoetzold@users.noreply.github.com>
2022-05-23 20:15:58 +0200

    feat: removed firebase

--- app/src/playstore/java/com/kamron/pogoiv/utils/CrashlyticsWrapperImpl.java
+++ app/src/playstore/java/com/kamron/pogoiv/utils/CrashlyticsWrapperImpl.java
@@ -1,15 +1,6 @@
 package com.kamron.pogoiv.utils;
 
 import android.content.Context;
-import android.text.TextUtils;
-
-import androidx.annotation.NonNull;
-
-import com.google.firebase.crashlytics.FirebaseCrashlytics;
-import com.kamron.pogoiv.BuildConfig;
-import com.kamron.pogoiv.GoIVSettings;
-
-import timber.log.Timber;
 
 public class CrashlyticsWrapperImpl extends CrashlyticsWrapper {
 
@@ -18,25 +9,7 @@ public class CrashlyticsWrapperImpl extends CrashlyticsWrapper {
 
     @Override
     public void init(Context context) {
-        if (BuildConfig.INTERNET_AVAILABLE && GoIVSettings.getInstance(context).shouldSendCrashReports()) {
-            FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true);
-
-            Timber.plant(new CrashReportingTree());
-        }
+        // Nothing to do
     }
 
-    private static class CrashReportingTree extends Timber.Tree {
-        private CrashReportingTree() {
-        }
-
-        @Override
-        protected void log(int priority, String tag, @NonNull String message, Throwable t) {
-            if (t != null) {
-                FirebaseCrashlytics.getInstance().recordException(t);
-            }
-            if (!TextUtils.isEmpty(message)) {
-                FirebaseCrashlytics.getInstance().log(message);
-            }
-        }
-    }
 }
